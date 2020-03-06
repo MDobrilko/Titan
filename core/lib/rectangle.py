62a8d957862082772a789f9ecee0cd185771107f
Михаил Добрилко <dobmisha@mail.ru>
2020-03-06 23:44:25 +0200

    fixed buildEnclosingRect function in Rectangle

--- core/lib/rectangle.py
+++ core/lib/rectangle.py
@@ -31,15 +31,13 @@ class Rectangle:
     
     
     def buildEnclosingRect(self, rect1, rect2):
-        point1 = Point(
+        self.__point1 = Point(
             min(rect1.getMinX(), rect2.getMinX()),
             min(rect1.getMinY(), rect2.getMinY()))
 
-        point2 = Point(
+        self.__point2 = Point(
             max(rect1.getMaxX(), rect2.getMaxX()),
             max(rect1.getMaxY(), rect2.getMaxY()))
-
-        self.__rect = Rectangle(point1, point2)
     
 
     def isCrossing(self, other):
